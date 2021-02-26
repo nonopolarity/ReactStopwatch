80e534127e50f65c1ad1cd1df7f7a269784d673c
Kin <winterheat@gmail.com>
2021-02-26 09:31:01 -0800

    ok

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -4,6 +4,9 @@ let startTime = null;
 
 export default function Stopwatch() {
     const [isStopwatchRunning, setIsStopWatchRunning] = useState(false);
+    // const [isStopwatchAtReset, setIsStopwatchAtReset] = useState(true);
+    const [cumulativeDuration, setCumulativeDuration] = useState(0);
+
     const [currentTime, setCurrentTime] = useState(0);
 
     useEffect(() => {
@@ -19,19 +22,35 @@ export default function Stopwatch() {
 
 
     function handleStart(ev) {
-        if (!isStopwatchRunning) {
+        if (!isStopwatchRunning) {  // Starting
             startTime = Date.now();
-            
-        } else {
+            // setIsStopwatchAtReset(false);
+        } else {    // Pausing
             // setIsStopWatchRunning(false);
+            setCumulativeDuration(cumulativeDuration + Date.now() - startTime);
         }
         setIsStopWatchRunning(!isStopwatchRunning);
     }
 
+    function handleReset() {
+        setIsStopWatchRunning(false);
+        setCumulativeDuration(0);
+    }
+
+    let displayedDuration;
+    if (startTime === null) {
+        displayedDuration = 0;
+    } else if (!isStopwatchRunning) {
+        displayedDuration = cumulativeDuration;
+    } else {
+        displayedDuration = cumulativeDuration + Date.now() - startTime;
+    }
+
     return (
         <>
-            <div>{ startTime === null ? 0 : ((Date.now() - startTime) / 1000).toFixed(2)}</div>
+            <div>{ (displayedDuration / 1000).toFixed(2)}</div>
             <button onClick={handleStart}>{isStopwatchRunning ? "Pause" : "Start"}</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 
