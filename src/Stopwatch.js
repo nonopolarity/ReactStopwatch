1d6f16cf786d47410b29565a4fac5c9f29f6a8ef
Kin <winterheat@gmail.com>
2021-02-26 13:08:34 -0800

    ok

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import "./Stopwatch.css";
 
-// Design goal: so that the displayed duration is not
-// affected by the asychronous update (render) behavior and setData()
+// Design goal: the displayed duration is not
+// affected by the asychronous update (render) behavior and any setData()
 // of ReactJS, but can reflect the most accurate time
 // between the time that the user presses Start and Pause
-// (and repeatedly doing so).
+// (and user repeatedly doing so).
 
 let startTime = null;
 
@@ -25,13 +25,10 @@ export default function Stopwatch() {
         }
     }, [isStopwatchRunning]);
 
-
-    function handleStart(ev) {
-        if (!isStopwatchRunning) {  // Starting it
+    function handleStartAndPause() {
+        if (!isStopwatchRunning) {    // Starting it
             startTime = Date.now();
-            // setIsStopwatchAtReset(false);
-        } else {    // Pausing it
-            // setIsStopWatchRunning(false);
+        } else {                      // Pausing it
             setCumulativeDuration(cumulativeDuration + Date.now() - startTime);
         }
         setIsStopWatchRunning(!isStopwatchRunning);
@@ -42,17 +39,28 @@ export default function Stopwatch() {
         setCumulativeDuration(0);
     }
 
-    let displayedDuration;
+    let duration;
     if (!isStopwatchRunning) {
-        displayedDuration = cumulativeDuration;
+        duration = cumulativeDuration;
     } else {
-        displayedDuration = cumulativeDuration + Date.now() - startTime;
+        duration = cumulativeDuration + Date.now() - startTime;
     }
+
+    function pad(n) {
+        return (n >= 10) ? `${n}` : `0${n}`;
+    }
+    const nDays = Math.floor(duration / 24 / 60 / 60 / 1000),
+        days = (nDays === 0) ? "" : `${nDays} days`,
+        hours = pad(Math.floor(duration / 60 / 60 / 1000) % 24),
+        minutes = pad(Math.floor(duration / 60 / 1000) % 60),
+        seconds = pad(Math.floor(duration / 1000) % 60),
+        hundredth = pad(Math.floor((duration % 1000) / 10));
 
     return (
         <div className="stopwatch">
-            <div className="duration-display">{(displayedDuration / 1000).toFixed(2)}</div>
-            <button onClick={handleStart}>{isStopwatchRunning ? "Pause" : "Start"}</button>
+            <div className="duration-display">{`${days} ${hours}:${minutes}:${seconds}`}<div>{hundredth} / 100</div></div>
+            {/* <div className="duration-display">{(duration / 1000).toFixed(2)}</div> */}
+            <button onClick={handleStartAndPause}>{isStopwatchRunning ? "Pause" : "Start"}</button>
             <button onClick={handleReset}>Reset</button>
         </div>
     );
